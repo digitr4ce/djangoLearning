e0a1de8a45989ceb7791843da9f71700ae5a571a
digitr4ce <liviusopon@gmail.com>
2024-10-09 22:13:06 +0300

    feat: add mixins for views

--- pbbg/game/views.py
+++ pbbg/game/views.py
@@ -3,50 +3,38 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from rest_framework import mixins
+from rest_framework import generics
+
 from .models import Player
 from .serializers import PlayerSerializer
 
-class PlayerList(APIView):
+class PlayerList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
     """
     List all players, or create a new player.
     """
-    def get(self, request, format=None):
-        players = Player.objects.all()
-        serializer = PlayerSerializer(players, many=True)
-        return Response(serializer.data)
-    
-    def post(self, request, format=None):
-        serializer = PlayerSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class PlayerDetail(APIView):
+class PlayerDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
     """
     Retrieve, update or delete a player instance.
     """
-    def get_object(self, player_id):
-        try:
-            return Player.objects.get(pk=player_id)
-        except Player.DoesNotExist:
-            raise Http404
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def get(self, request, player_id, format=None):
-        player = self.get_object(player_id)
-        serializer = PlayerSerializer(player)
-        return Response(serializer.data)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def put(self, request, player_id, format=None):
-        player = self.get_object(player_id)
-        serializer = PlayerSerializer(player, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, player_id, format=None):
-        player = self.get_object(player_id)
-        player.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
