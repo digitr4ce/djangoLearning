a1dfaf52995de37f505083d08cfd8c22f740514f
digitr4ce <liviusopon@gmail.com>
2024-10-09 22:14:18 +0300

    feat: change to api view based on generics

--- pbbg/game/views.py
+++ pbbg/game/views.py
@@ -1,40 +1,18 @@
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-
-from rest_framework import mixins
 from rest_framework import generics
 
 from .models import Player
 from .serializers import PlayerSerializer
 
-class PlayerList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class PlayerList(generics.ListCreateAPIView):
     """
     List all players, or create a new player.
     """
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class PlayerDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class PlayerDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a player instance.
     """
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
-    
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
