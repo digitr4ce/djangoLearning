85a6c8e869256d7472365c6370335f166d1bb8da
digitr4ce <liviusopon@gmail.com>
2024-10-12 00:19:01 +0300

    feat: add nested relationships in serializers

--- pbbg/game/serializers.py
+++ pbbg/game/serializers.py
@@ -1,16 +1,29 @@
 from rest_framework import serializers
-from game.models import Player
+from game.models import Player, InventoryItems, Inventory
 from django.contrib.auth.models import User
+
+class InventoryItemsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InventoryItems
+        fields = ['id', 'resource_type', 'quantity']
+
+class InventorySerializer(serializers.ModelSerializer):
+    inventory_items = InventoryItemsSerializer(many=True)
+
+    class Meta:
+        model = Inventory
+        fields = ['id', 'inventory_items']
+
 
 class PlayerSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    inventory = InventoryItemsSerializer()
 
     class Meta:
         model = Player
-        fields = ['id', 'name', 'health', 'hunger', 'owner']
+        fields = ['id', 'hunger', 'owner', 'inventory']
 
 class UserSerializer(serializers.ModelSerializer):
-    # TODO: Rename Payers to Characters
     players = serializers.PrimaryKeyRelatedField(many=True, queryset=Player.objects.all())
 
     class Meta:
